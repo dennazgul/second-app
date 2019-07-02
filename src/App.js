4ccc1b5fdb8915e35b3554bf1bee51c1f4d737f6
Denis Baranov <nazguldragon@mail.ru>
2019-07-02 10:57:37 +0300

    proj

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: 'fefef' };
+  }
+  handleTextareaChange(event) {
+    this.setState({ value: event.target.value });
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="App-logo">
+            <textarea value={this.state.value} name="comment" onChange={this.handleTextareaChange} cols="40" rows="3"></textarea>
+          </div>
+          <span>
+            <a href="https://google.com"
+              className="App-link">
+              Вход</a>
+          </span>
+          <span>
+            <a href="https://google.com"
+              className="App-link">
+              Регистрация
+          </a>
+          </span>
+        </header>
+      </div>
+    );
+  }
 }
-
-export default App;
+export default NameForm;
